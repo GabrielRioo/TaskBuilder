16868414309ee5233654e44cd05d6b51ab5d32d5
GabrielSouza <gabriel.souza@telecall.com>
2020-04-06 12:18:07 -0300

    Adding javascript code to send the message with the key 'Enter' (review)

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -10,7 +10,7 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
-// Quando clicar no 'x', apaga a tarefa 
+// Quando clicar no 'x', apaga a tarefa
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
@@ -47,6 +47,8 @@ function newElement() {
   span.appendChild(txt);
   li.appendChild(span);
 
+  
+
   for (i = 0; i < close.length; i++) {
 	close[i].onclick = function() {
 	  var div = this.parentElement;
@@ -56,14 +58,39 @@ function newElement() {
 }
 
   //=============Enviar com Enter==============
-  // const inputEle = document.getElementById('myInput');
-  // inputEle.addEventListener('keyup', function(e){
-  //   var key = e.which || e.keyCode;
-  //   if (key == 13) { // codigo da tecla enter
-  //     //adicionar a função aqui
-  //     alert('carregou enter o valor digitado foi: ' +this.value);
-  //   }
-  // });
+  const inputEle = document.getElementById('myInput');
+  inputEle.addEventListener('keyup', function(e){
+    var key = e.which || e.keyCode;
+    if (key == 13) { // codigo da tecla enter
+      var li = document.createElement("li");
+      var inputValue = document.getElementById("myInput").value;
+      var t = document.createTextNode(inputValue);
+      li.appendChild(t);
+      if (inputValue === '') {
+      alert("Você precisa escrever alguma tarefa!");
+      } else {
+      document.getElementById("myUL").appendChild(li);
+      }
+      document.getElementById("myInput").value = "";
+
+      var span = document.createElement("SPAN");
+      var txt = document.createTextNode("\u00D7");
+      span.className = "close";
+      span.appendChild(txt);
+      li.appendChild(span);
+
+      
+
+      for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+      }
+      // alert('carregou enter o valor digitado foi: ' +this.value);
+
+    }
+  });
 
 //============================DRAG AND DROP================================
 
